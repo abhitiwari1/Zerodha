9b8c087776283f5e6988548b92db554487ba19ed
abhitiwari1 <151872523+abhitiwari1@users.noreply.github.com>
2025-06-10 22:42:29 +0530

    Delete backend directory

--- backend/config/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-function connectToDB () {
-    mongoose.connect(process.env.MONGO_URL).then(() =>{
-        console.log('Connected to mongoose successfully');
-    }).catch( err => console.log(err));
-}
-
-module.exports = connectToDB;
--- backend/server.js
+++
@@ -1,8 +0,0 @@
-const http = require('http')
-const app = require('./app')
-const port = process.env.PORT || 8080
-const server = http.createServer(app)
-
-server.listen(port, () =>{
-    console.log(`Sever is listening on port ${port}`);
-})
