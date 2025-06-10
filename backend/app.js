db3f8b2387690e0f7a331ac40d17de8a85159907
abhitiwari1 <151872523+abhitiwari1@users.noreply.github.com>
2025-06-10 23:02:57 +0530

    backened

---
+++ backend/app.js
@@ -0,0 +1,21 @@
+// const dotenv = require('dotenv')
+// dotenv.config()
+// const express = require('express')
+// const cors = require('cors')
+// const cookieParser = require('cookie-parser')
+// const connectToDB = require('./config/db')
+
+// connectToDB();
+
+// const app = express()
+// app.use(cors())
+// app.use(express.json())
+// app.use(express.urlencoded({extended: true}))
+// app.use(cookieParser())
+
+// app.get('/',(req, res) => {
+//     res.send('Hello World!')
+// })
+
+
+// module.exports = app;
---
+++ backend/config/db.js
@@ -0,0 +1,9 @@
+// const mongoose = require('mongoose')
+
+// function connectToDB () {
+//     mongoose.connect(process.env.MONGO_URL).then(() =>{
+//         console.log('Connected to mongoose successfully');
+//     }).catch( err => console.log(err));
+// }
+
+// module.exports = connectToDB;
