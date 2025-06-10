59bbec73efe84ac344517263319c40970684ffc9
abhitiwari1 <151872523+abhitiwari1@users.noreply.github.com>
2025-06-10 22:48:12 +0530

    Zerodha backened

--- dashboard/src/components/Dashboard.jsx
+++ dashboard/src/components/Dashboard.jsx
@@ -18,12 +18,12 @@ export default function Dashboard() {
             </GeneralContextProvider>
             <div className="content">
                 <Routes>
-                <Route exact path="/" element={<Summary />} />
-                <Route path="/orders" element={<Orders />} />
-                <Route path="/holdings" element={<Holdings />} />
-                <Route path="/positions" element={<Positions />} />
-                <Route path="/funds" element={<Funds />} />
-                <Route path="/apps" element={<Apps />} />
+                    <Route exact path="/" element={<Summary />} />
+                    <Route path="/orders" element={<Orders />} />
+                    <Route path="/holdings" element={<Holdings />} />
+                    <Route path="/positions" element={<Positions />} />
+                    <Route path="/funds" element={<Funds />} />
+                    <Route path="/apps" element={<Apps />} />
                 </Routes>
             </div>
         </div>
--- dashboard/src/components/Orders.jsx
+++ dashboard/src/components/Orders.jsx
@@ -1,5 +1,18 @@
-export default function Orders() {
-    return (
-        <div>Orders</div>
-    );
-}
+
+import { Link } from "react-router-dom";
+
+const Orders = () => {
+  return (
+    <div className="orders">
+      <div className="no-orders">
+        <p>You haven't placed any orders today</p>
+
+        <Link to={"/"} className="btn">
+          Get started
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Orders;
