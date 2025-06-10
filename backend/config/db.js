f188f24fb2bc8e76a189ec72f0457d64de251080
abhitiwari1 <151872523+abhitiwari1@users.noreply.github.com>
2025-06-10 22:56:43 +0530

    Zerodha backened

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,9 @@
-// const mongoose = require('mongoose')
+const mongoose = require('mongoose')
 
-// function connectToDB () {
-//     mongoose.connect(process.env.MONGO_URL).then(() =>{
-//         console.log('Connected to mongoose successfully');
-//     }).catch( err => console.log(err));
-// }
+function connectToDB () {
+    mongoose.connect(process.env.MONGO_URL).then(() =>{
+        console.log('Connected to mongoose successfully');
+    }).catch( err => console.log(err));
+}
 
-// module.exports = connectToDB;
+module.exports = connectToDB;
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
-// const http = require('http')
-// const app = require('./app')
-// const port = process.env.PORT || 8080
-// const server = http.createServer(app)
+const http = require('http')
+const app = require('./app')
+const port = process.env.PORT || 8080
+const server = http.createServer(app)
 
-// server.listen(port, () =>{
-//     console.log(`Sever is listening on port ${port}`);
-// })
+server.listen(port, () =>{
+    console.log(`Sever is listening on port ${port}`);
+})
